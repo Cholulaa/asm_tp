71ad381d323b1debf43a140b156e5942cc692844
SAIDI Mohamed <106685748+Cholulaa@users.noreply.github.com>
2025-02-04 14:50:28 +0100

    Update asm04.s

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -1,46 +1,45 @@
 section .bss
-    user_input resb 6
+    number resb 10  
 
 section .text
     global _start
 
 _start:
-    mov eax, 0
-    mov edi, 0
-    mov esi, user_input
-    mov edx, 6
+    mov rax, 0          
+    mov rdi, 0          
+    mov rsi, number     
+    mov rdx, 10         
     syscall
 
-    sub byte [esi + eax - 1], '0'
-    test eax, eax
-    jz .exit
+    mov rsi, number
+    call str_to_int
 
-    xor ecx, ecx
-    xor ebx, ebx
-.convert_loop:
-    movzx eax, byte [esi + ecx]
-    cmp eax, '0'
-    jl .done
-    sub eax, '0'
-    imul ebx, ebx, 10
-    add ebx, eax
-    inc ecx
-    jmp .convert_loop
+    test rax, 1          
+    jz return_zero       
+
+return_one:
+    mov rax, 60         
+    mov rdi, 1          
+    syscall
+
+return_zero:
+    mov rax, 60         
+    mov rdi, 0          
+    syscall
+
+str_to_int:
+    xor rax, rax        
+    xor rcx, rcx        
+
+.loop:
+    movzx rdx, byte [rsi]  
+    cmp rdx, 10        
+    je .done
+    sub rdx, '0'       
+    imul rax, rax, 10  
+    add rax, rdx       
+    inc rsi            
+    jmp .loop
+
 .done:
-
-    test ebx, 2
-    jnp .is_even
-
-    mov eax, 60
-    mov edi, 1
-    syscall
-
-.is_even:
-    mov eax, 60
-    xor edi, edi
-    syscall
-
-.exit:
-    mov eax, 60
-    xor edi, edi
-    syscall
+    ret
