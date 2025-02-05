7c1b27122c1bec09297cb55054aa60430ca7d811
Mohamed Saidi <saidimohamed2603@gmail.com>
2025-02-05 16:23:18 +0100

    ver final

--- asm08/asm08.s
+++ asm08/asm08.s
@@ -4,90 +4,59 @@ buffer: times 64 db 0
 section .text
 global _start
 
-; asm08: Somme des entiers < n
-; Exemples:
-;   ./asm08 5   => 1+2+3+4 = 10
-;   ./asm08 1   => 0
-;   ./asm08 10  => 45
-; Retourne 0 en cas de succès, ou 1 s'il y a une erreur (ex: pas de param).
-
 _start:
-    ; Vérifie qu'on a un paramètre (argv[1])
     mov rsi, [rsp+16]
     test rsi, rsi
-    jz error
-
-    ; Parse l'entier => RAX
+    jz e
     call parse
-
-    ; Si n <= 1 => somme = 0
     cmp rax, 2
-    jb .zero
-
-    ; Sinon somme(1..(n-1)) = n*(n-1)/2
+    jb z
     mov rbx, rax
     dec rbx
-    mul rbx       ; RDX:RAX = n*(n-1)
+    mul rbx
     xor rdx, rdx
     mov rcx, 2
-    div rcx       ; RAX = (n*(n-1))/2
-    jmp .print
-
-.zero:
-    xor rax, rax
-
-.print:
+    div rcx
+z:
     call print
     mov rax, 60
     xor rdi, rdi
     syscall
-
-error:
+e:
     mov rax, 60
     mov rdi, 1
     syscall
 
-; -----------------------------------------------------------------------------
-; parse:
-;   RSI => pointeur sur argv[1]
-;   RAX => l'entier parsé (>= 0)
-;   Si pas de digit, on renvoie 0 comme fallback (pour éviter un exit(1) forcé)
-; -----------------------------------------------------------------------------
 parse:
     xor rax, rax
-.parse_loop:
+pl:
     mov dl, [rsi]
     test dl, dl
-    jz .done
+    jz d
     cmp dl, 10
-    je .done
+    je d
     sub dl, '0'
-    jl .done
+    jl d
     cmp dl, 9
-    jg .done
+    jg d
     imul rax, rax, 10
     add rax, rdx
     inc rsi
-    jmp .parse_loop
-.done:
+    jmp pl
+d:
     ret
 
-; -----------------------------------------------------------------------------
-; print:
-;   Affiche RAX en ASCII (décimal) sur stdout
-; -----------------------------------------------------------------------------
 print:
     test rax, rax
-    jnz .convert
+    jnz cv
     mov byte [buffer], '0'
     mov rsi, buffer
     mov rdx, 1
-    jmp .write
-
-.convert:
+    jmp w
+cv:
     mov rbx, rax
     lea rdi, [buffer+63]
-.conv_loop:
+lp:
     xor rdx, rdx
     mov rax, rbx
     mov rcx, 10
@@ -97,13 +66,12 @@ print:
     mov byte [rdi], dl
     dec rdi
     test rbx, rbx
-    jnz .conv_loop
+    jnz lp
     inc rdi
     mov rsi, rdi
     mov rdx, buffer+64
     sub rdx, rdi
-
-.write:
+w:
     mov rax, 1
     mov rdi, 1
     syscall
