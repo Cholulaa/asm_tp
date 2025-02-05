3b38a4e12f46f419e9aab2ec7f2943a8ca9f4f08
Mohamed Saidi <saidimohamed2603@gmail.com>
2025-02-05 16:48:24 +0100

    ver fin

--- asm10/asm10.s
+++ asm10/asm10.s
@@ -5,83 +5,110 @@ section .text
 global _start
 
 _start:
-    xor rax, rax
-    mov rdi, rax
-    mov rsi, buffer
-    mov rdx, 64
-    syscall
+    mov r8, [rsp]
+    cmp r8, 4
+    jl e
+    mov rsi, [rsp+16]
+    call parse
+    mov rbx, rax
+    mov rsi, [rsp+24]
+    call parse
     mov rcx, rax
-    xor rbx, rbx
-.loop:
-    test rcx, rcx
-    jz .done
-    mov al, [rsi]
-    inc rsi
-    dec rcx
-    call is_vowel
-    add rbx, rax
-    jmp .loop
-.done:
+    mov rsi, [rsp+32]
+    call parse
+    mov rdx, rax
+    cmp rcx, rbx
+    jle .c
+    mov rbx, rcx
+.c:
+    cmp rdx, rbx
+    jle .m
+    mov rbx, rdx
+.m:
     mov rax, rbx
-    call print
+    call print_signed
     mov rax, 60
     xor rdi, rdi
     syscall
+e:
+    mov rax, 60
+    mov rdi, 1
+    syscall
 
-is_vowel:
-    cmp al, 'a'
-    je .yes
-    cmp al, 'e'
-    je .yes
-    cmp al, 'i'
-    je .yes
-    cmp al, 'o'
-    je .yes
-    cmp al, 'u'
-    je .yes
-    cmp al, 'A'
-    je .yes
-    cmp al, 'E'
-    je .yes
-    cmp al, 'I'
-    je .yes
-    cmp al, 'O'
-    je .yes
-    cmp al, 'U'
-    je .yes
+parse:
+    xor r8, r8
+    mov dl, [rsi]
+    cmp dl, '-'
+    jne d
+    mov r8, 1
+    inc rsi
+d:
     xor rax, rax
-    ret
-.yes:
-    mov rax, 1
+l:
+    mov dl, [rsi]
+    test dl, dl
+    jz .done
+    cmp dl, 10
+    je .done
+    sub dl, '0'
+    jl .done
+    cmp dl, 9
+    jg .done
+    imul rax, rax, 10
+    add rax, rdx
+    inc rsi
+    jmp l
+.done:
+    test r8, r8
+    jz r
+    neg rax
+r:
     ret
 
-print:
+print_signed:
     test rax, rax
-    jnz .conv
-    mov byte [buffer], '0'
-    mov rsi, buffer
-    mov rdx, 1
-    jmp .wr
-.conv:
-    mov rbx, rax
-    lea rdi, [buffer+63]
-.next:
-    xor rdx, rdx
-    mov rax, rbx
-    mov rcx, 10
-    div rcx
-    mov rbx, rax
-    add rdx, '0'
-    mov byte [rdi], dl
-    dec rdi
-    test rbx, rbx
-    jnz .next
+    jns .p
+    mov byte [buffer], '-'
+    neg rax
+    lea rdi, [buffer+1]
+    jmp c
+.p:
+    lea rdi, [buffer]
+c:
+    mov rcx, rax
+    test rcx, rcx
+    jnz .v
+    mov byte [rdi], '0'
     inc rdi
-    mov rsi, rdi
-    mov rdx, buffer+64
-    sub rdx, rdi
-.wr:
+    jmp w
+.v:
+    lea rsi, [buffer+63]
+lp:
+    xor rdx, rdx
+    mov rax, rcx
+    mov r8, 10
+    div r8
+    mov rcx, rax
+    add rdx, '0'
+    mov byte [rsi], dl
+    dec rsi
+    test rcx, rcx
+    jnz lp
+    inc rsi
+    mov rax, buffer+64
+    sub rax, rsi
+cp:
+    mov dl, [rsi]
+    mov [rdi], dl
+    inc rdi
+    inc rsi
+    dec rax
+    jnz cp
+w:
     mov rax, 1
+    mov rsi, buffer
+    sub rdi, buffer
+    mov rdx, rdi
     mov rdi, 1
     syscall
     ret
