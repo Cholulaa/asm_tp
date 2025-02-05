a46f106931036487e84bb1470b146cf4da1b6435
Mohamed Saidi <saidimohamed2603@gmail.com>
2025-02-05 16:08:11 +0100

    first test

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -1,130 +1,88 @@
-global _start 
-
-section .bss
-    nb resb 32
-
 section .data
-    help: db "Add numbers from 0 to NUMBER-1", 10
-    .lenHelp: equ $ - help
-    usage: db "USAGE : ./asm07 NUMBER", 10
-    .lenUsage: equ $ - usage
+buf: times 32 db 0
 
 section .text
+global _start
+
+; Program:
+;  1) Reads up to 32 bytes from stdin into 'buf'
+;  2) parse() => RAX = integer
+;  3) if prime => return 0, else 1
+;  4) exit with that code
+
 _start:
-
-    mov r13, [rsp]
-    cmp r13, 0x2
-    jne _error
-
-    mov rsi, rsp
-    add rsi, 16
-    mov rsi, [rsi]
-    mov rdi, nb
-    mov rcx, 4
-    rep movsb
-
-    xor rdi, rdi
-    mov r8, 0
-
-convert:
-    mov al, [nb + rdi]
-    cmp al, 0
-    je done
-
-    cmp rax, '0'
-    jl _error
-
-    cmp rax, '9'
-    jg _error
-
-    sub rax, 48
-    imul r8, 10
-    add r8, rax
-    
-    inc rdi
-    jmp convert
-
-done:
-    cmp r8, 0
-    je _end
-    
-    mov r9, 0
-    mov rax, 0
-    dec r8
-
-loop:
-    add rax, r9
-    
-    cmp r9, r8
-    je _end
-
-    inc r9
-    jmp loop
-
-_end:
-    call std__to_string
-    mov rax, 1
-    mov rdi, 1
+    xor rax, rax        ; sys_read
+    mov rdi, rax        ; stdin = 0
+    mov rsi, buf
+    mov rdx, 32
     syscall
 
+    call parse
+    mov rdi, rax        ; store input in RDI for prime check
+
+    ; is_prime(RDI) => RAX=0 if prime, 1 if not
+    call is_prime
+
+    ; sys_exit(RAX)
+    mov rdi, rax
     mov rax, 60
-    mov rdi, 0
     syscall
 
-_error:
+; parse => RAX = integer from buf
+parse:
+    xor rax, rax
+parse_loop:
+    mov dl, [rsi]
+    cmp dl, 0
+    je parse_done
+    cmp dl, 10
+    je parse_done
+    sub dl, '0'
+    jl parse_done
+    cmp dl, 9
+    jg parse_done
+    imul rax, rax, 10
+    add rax, rdx
+    inc rsi
+    jmp parse_loop
+parse_done:
+    ret
+
+; is_prime(RDI):
+;   RDI = input number
+;   RAX = 0 if prime, 1 if not
+is_prime:
+    cmp rdi, 2
+    jb  not_prime        ; <2 => not prime
+
+    ; We'll do a naive loop from 2..(RDI-1) or up to sqrt(RDI) (whichever).
+    ; For brevity, let's do up to sqrt(RDI).
+    ; We store RDI in RBX, use RCX as loop variable: 2.. up to sqrt(RBX)
+    mov rbx, rdi
+    mov rcx, 2
+prime_loop:
+    ; if rcx*rcx > rbx => prime
+    mov rax, rcx
+    mul rcx              ; 128-bit multiply: RDX:RAX = RCX^2
+    cmp rdx, 0
+    jne prime_yes        ; overflow => definitely bigger than rbx => prime
+    cmp rax, rbx
+    ja  prime_yes
+
+    ; check divisibility
+    mov rax, rbx
+    xor rdx, rdx
+    div rcx
+    test rdx, rdx
+    jz not_prime         ; remainder=0 => not prime
+
+    inc rcx
+    jmp prime_loop
+
+prime_yes:
+    xor rax, rax
+    ret
+
+not_prime:
     mov rax, 1
-    mov rdi, 1
-    mov rsi, help
-    mov rdx, help.lenHelp
-    syscall
-    
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, usage
-    mov rdx, usage.lenUsage
-    syscall
-
-    mov rax, 60
-    mov rdi, 1
-    syscall
-
-std__to_string:
-    push rsi
-    push rax
-
-    mov rdi, 1
-    mov rcx, 1
-    mov rbx, 10
-
-    .get_divisor:
-        xor rdx, rdx
-        div rbx
-        cmp rax, 0
-        je ._after
-        imul rcx, 10
-        inc rdi
-        jmp .get_divisor
-
-    ._after:
-        pop rax
-        push rdi
-
-    .to_string:
-        xor rdx, rdx
-        div rcx
-        add al, '0'
-        mov [rsi], al
-        inc rsi
-        push rdx
-        xor rdx, rdx
-        mov rax, rcx
-        mov rbx, 10
-        div rbx
-        mov rcx, rax
-        pop rax
-        cmp rcx, 0
-        jg .to_string
-
-    pop rdx
-    pop rsi
     ret
