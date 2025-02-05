235658ef41bd9d2de2a360dcc306570a5ca673e9
Mohamed Saidi <saidimohamed2603@gmail.com>
2025-02-05 18:18:16 +0100

    ver1 asm 14

---
+++ asm14/asm14.s
@@ -0,0 +1,34 @@
+section .data
+msg: db "Hello Universe!", 0Ah
+len: equ $ - msg
+
+section .text
+global _start
+
+_start:
+    mov r8, [rsp]
+    cmp r8, 2
+    jl e
+    mov rdi, [rsp+16]
+    mov rax, 2
+    mov rsi, 577
+    mov rdx, 0o644
+    syscall
+    cmp rax, 0
+    js e
+    mov rbx, rax
+    mov rax, 1
+    mov rdi, rbx
+    mov rsi, msg
+    mov rdx, len
+    syscall
+    mov rax, 3
+    mov rdi, rbx
+    syscall
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+e:
+    mov rax, 60
+    mov rdi, 1
+    syscall
