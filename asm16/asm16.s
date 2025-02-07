fa5f5013bf3d8aa2c785790abbd81b9b5b4a0d9c
Mohamed Saidi <saidimohamed2603@gmail.com>
2025-02-07 11:58:55 +0100

    ver4

--- asm16/asm16.s
+++ asm16/asm16.s
@@ -1,89 +1,50 @@
-section .data
-    search db "1337"
-    replace db "H4CK"
+; Read file content
+mov rdi, rbx        ; fd
+mov rax, 0          ; sys_read
+mov rsi, buffer     ; buffer
+mov rdx, 1024       ; count
+syscall
 
-section .text
-    global _start
+; Check read success and save the count
+test rax, rax
+jle .close
+mov rcx, rax        ; Save number of bytes read in rcx
 
-_start:
-    ; Check argc (number of arguments)
-    mov rdi, [rsp]      ; Get argc
-    cmp rdi, 2          ; Need at least 2 (program name + 1 argument)
-    jl .error
-
-    ; Get argv[1] - filename
-    mov rdi, [rsp + 16] ; Get pointer to filename
-
-    ; Open file
-    mov rax, 2          ; sys_open
-    mov rsi, 2          ; O_RDWR
-    xor rdx, rdx        ; mode
-    syscall
-
-    ; Check if open succeeded
-    test rax, rax
-    js .error
-    mov rbx, rax        ; Save fd
-
-    ; Read file content
-    mov rdi, rbx        ; fd
-    mov rax, 0          ; sys_read
-    mov rsi, buffer     ; buffer
-    mov rdx, 1024       ; count
-    syscall
-
-    ; Check read success
-    test rax, rax
-    jle .close
-
-    ; Search for pattern
-    mov rsi, buffer     ; Source
-    xor rdx, rdx        ; Offset counter
+; Search for pattern
+mov rsi, buffer     ; Source pointer
+xor rdx, rdx        ; Offset counter
 
 .loop:
-    mov eax, [search]   ; Load search pattern
-    cmp [rsi], eax      ; Compare with current position
-    je .replace         ; Found it!
+    mov eax, [search]   ; Load search pattern into eax
+    cmp [rsi], eax      ; Compare current 4 bytes with the pattern
+    je .replace         ; If equal, jump to replacement
 
-    inc rsi             ; Next position
+    inc rsi             ; Advance the pointer
     inc rdx             ; Increment offset
-    cmp rdx, rax        ; Check if we reached end
+    cmp rdx, rcx        ; Compare offset to number of bytes read
     jl .loop
-    jmp .close          ; Pattern not found
+    jmp .close          ; Pattern not found, so close file
 
 .replace:
-    ; Seek to position
+    ; Seek to the found position
     mov rdi, rbx        ; fd
     mov rax, 8          ; sys_lseek
     mov rsi, rdx        ; offset
-    xor rdx, rdx        ; SEEK_SET
+    xor rdx, rdx        ; SEEK_SET (0)
     syscall
 
-    ; Write replacement
+    ; Write replacement string
     mov rdi, rbx        ; fd
     mov rax, 1          ; sys_write
-    mov rsi, replace    ; buffer
-    mov rdx, 4          ; count
+    mov rsi, replace    ; replacement buffer ("H4CK")
+    mov rdx, 4          ; number of bytes to write
     syscall
 
     ; Close file and exit successfully
     mov rdi, rbx
     mov rax, 3          ; sys_close
     syscall
-    
-    xor rdi, rdi        ; Success exit code
+
+    xor rdi, rdi        ; Success exit code (0)
     mov rax, 60         ; sys_exit
     syscall
-
-.close:
-    mov rdi, rbx
-    mov rax, 3          ; sys_close
-    syscall
-
-.error:
-    mov rdi, 1          ; Error exit code
-    mov rax, 60         ; sys_exit
-    syscall
-
-section .bss
-    buffer resb 1024
