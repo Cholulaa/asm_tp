82633184ddf793d907d49618fc3ac160df4bf204
Mohamed Saidi <saidimohamed2603@gmail.com>
2025-03-05 16:27:57 +0100

    Revert "ver13"
    This reverts commit 7fb20d026cf2c787171ac9577305ee27431d1976.

--- asm20/asm20.s
+++ asm20/asm20.s
@@ -152,39 +152,28 @@ cmd_echo:
     jmp client_loop
 
 cmd_reverse:
-    ; Calculate length of string to reverse (after "REVERSE ")
+    ; Skip "REVERSE " (8 chars)
     mov rcx, r14
-    sub rcx, 8          ; Skip "REVERSE " prefix
-    
-    ; Clear destination buffer
-    push rcx
-    mov rcx, r14
-    lea rdi, [revbuf]
-    xor rax, rax
-    rep stosb
-    pop rcx
-    
-    ; Setup source and destination
-    lea rsi, [buffer + 8]  ; Source: after "REVERSE "
+    sub rcx, 8          ; String length
+    lea rsi, [buffer + 8]  ; Source
     lea rdi, [revbuf]      ; Destination
-    add rsi, rcx          ; Point to end of source string
+    std                    ; Set direction flag for reverse copy
+    add rsi, rcx          ; Point to end of string
     dec rsi
-
+    
 .reverse_loop:
-    mov al, [rsi]         ; Get character from end
-    mov [rdi], al         ; Store at beginning
-    dec rsi               ; Move backward in source
-    inc rdi               ; Move forward in destination
+    lodsb
+    stosb
     loop .reverse_loop
+    cld                    ; Clear direction flag
 
     ; Send reversed string
-    mov rax, 1           ; sys_write
-    mov rdi, r13         ; client socket
-    lea rsi, [revbuf]    ; reversed string
-    mov rdx, r14         ; length
-    sub rdx, 8           ; subtract "REVERSE " length
+    mov rax, 1
+    mov rdi, r13
+    lea rsi, [revbuf]
+    mov rdx, r14
+    sub rdx, 8
     syscall
-    
     ; Add newline
     mov rax, 1
     mov rdi, r13
